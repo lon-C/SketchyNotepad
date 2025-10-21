a4d6e969c02ca65e75081dd0070baafbfb50aeaf
Ion <ionelu1998@gmail.com>
2025-10-21 18:30:12 +0100

    When a div is hovered upon, it turns black

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const container = document.querySelector('.container');
 for(let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
     div.className = 'box';
-    div.textContent = `Box ${i}`;
+    div.id = `${i}`;
     container.appendChild(div);
 }
+
+container.addEventListener("mouseover", (event) => {
+    event.target.className = 'hovered-box';
+});
+
