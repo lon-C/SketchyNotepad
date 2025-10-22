5d63ae5613525d867ecc5aa9a3c76fdfab9bebec
Ion <ionelu1998@gmail.com>
2025-10-22 17:44:38 +0100

    boxes sizes are updated in response to the number of boxes

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const container = document.querySelector('.container');
 
-for(let i = 1; i <= 256; i++) {
+const squaresPerSide = 20;
+
+const flexBasis = 100 / squaresPerSide +"%";
+
+for(let i = 1; i <= squaresPerSide*squaresPerSide; i++) {
     const div = document.createElement('div');
     div.className = 'box';
     div.id = `${i}`;
+    div.textContent = `${i}`;
     container.appendChild(div);
 }
 
 container.addEventListener("mouseover", (event) => {
     event.target.className = 'hovered-box';
+});
+
+//Boxes size is being updated after the number of boxes is chosen:
+document.querySelectorAll(".box").forEach(box => {
+    box.style.flex = `0 0 ${flexBasis}`;
 });
 
